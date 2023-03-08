c9ab7e1690420527440a649063f6766a41d4f980
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-03-08 21:47:14 +0000

    working version

--- glass_compat_numpy/__init__.py
+++ glass_compat_numpy/__init__.py
@@ -7,8 +7,8 @@ def random_namespace(*objs):
     if 'numpy' not in sys.modules:
         raise TypeError
     import numpy as np
-    for obj in objects:
-        if not isinstance(obj, np.random.Generator):
+    for obj in objs:
+        if not isinstance(obj, (np.ndarray, np.generic, np.random.Generator)):
             raise TypeError
     from . import random
     return random
@@ -18,7 +18,7 @@ def healpix_namespace(*objs):
     if 'numpy' not in sys.modules:
         raise TypeError
     import numpy as np
-    for obj in objects:
+    for obj in objs:
         if not isinstance(obj, (np.ndarray, np.generic)):
             raise TypeError
     from . import healpix
--- glass_compat_numpy/random.py
+++ glass_compat_numpy/random.py
@@ -13,21 +13,21 @@ def split(rand):
     return rand, rand
 
 
-def standard_normal(rand, shape=()):
+def standard_normal(rand, shape=None):
     return rand, rand.standard_normal(size=shape)
 
 
-def choice(rand, shape=(), replace=True, p=None, axis=0):
+def choice(rand, shape=None, replace=True, p=None, axis=0):
     return rand, rand.choice(size=shape, replace=replace, p=p, axis=axis)
 
 
-def uniform(rand, low=0.0, high=1.0, shape=()):
+def uniform(rand, low=0.0, high=1.0, shape=None):
     return rand, rand.uniform(low=low, high=high, size=shape)
 
 
-def normal(rand, loc=0.0, scale=1.0, shape=()):
+def normal(rand, loc=0.0, scale=1.0, shape=None):
     return rand, rand.normal(loc=loc, scale=scale, size=shape)
 
 
-def poisson(rand, lam=1.0, shape=()):
+def poisson(rand, lam=1.0, shape=None):
     return rand, rand.poisson(lam=lam, size=shape)
