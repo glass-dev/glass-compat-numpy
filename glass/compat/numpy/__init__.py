bf11243ebcd5aaa97cf46be2010d7a5f69dc813e
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-03-14 22:44:03 +0000

    unified array_namespace

--- glass/compat/numpy/__init__.py
+++ glass/compat/numpy/__init__.py
@@ -3,23 +3,12 @@
 import sys
 
 
-def random_namespace(*objs):
+def array_namespace(*objs):
     if 'numpy' not in sys.modules:
         raise TypeError
     import numpy as np
     for obj in objs:
         if not isinstance(obj, (np.ndarray, np.generic, np.random.Generator)):
             raise TypeError
-    from . import random
-    return random
-
-
-def healpix_namespace(*objs):
-    if 'numpy' not in sys.modules:
-        raise TypeError
-    import numpy as np
-    for obj in objs:
-        if not isinstance(obj, (np.ndarray, np.generic)):
-            raise TypeError
-    from . import healpix
-    return healpix
+    from . import array
+    return array
